2f280d3a771a5ba0328a9392a5417279316e96b5
Isbarcar <ismael.barranco.2402@fernando3martos.com>
2025-03-06 10:05:23 +0100

    prueba cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express= require ('express');
 
 const cors= require('cors')
+const corsOptions = {
+    origin: "*", // O usa '*' si quieres permitir cualquier origen
+    methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders: "Content-Type,Authorization",
+  };
 const app = express()
 const PORT= process.env.PORT || 3000;
 const v1CarRoutes= require("./v1/routes/carsRoutes");
@@ -11,7 +16,7 @@ const bodyParser = require('body-parser');
 app.get("/",(request,response)=>{
     response.send('<h1>HOLA CHURROS</h1>')
 })
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api/v1/cars',v1CarRoutes)
 
