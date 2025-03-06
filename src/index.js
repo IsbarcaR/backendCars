fe66c6c2d3b94096217b221cf4be33d3fb49afa6
Isbarcar <ismael.barranco.2402@fernando3martos.com>
2025-03-06 10:43:34 +0100

    Vuelta atras

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express= require ('express');
 
 const cors= require('cors')
-/* const corsOptions = {
+const corsOptions = {
     origin: "*", 
     methods: "GET,POST,PUT,DELETE,PATCH",
     allowedHeaders: "Content-Type,Authorization",
-  }; */
+  };
 const app = express()
 const PORT= process.env.PORT || 3000;
 const v1CarRoutes= require("./v1/routes/carsRoutes");
@@ -16,8 +16,8 @@ const bodyParser = require('body-parser');
 app.get("/",(request,response)=>{
     response.send('<h1>HOLA CHURROS</h1>')
 })
-app.use(cors({ origin: 'https://cars-fw4uia7pz-ismaels-projects-2bab6c89.vercel.app' }));
-/* app.use(cors(corsOptions)); */
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use('/api/v1/cars',v1CarRoutes)
 
